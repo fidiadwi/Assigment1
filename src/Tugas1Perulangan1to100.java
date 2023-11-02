688896be929b8125828e68c37d6330f255b5c323
fidiadwi <fidiadwii8@gmail.com>
2023-11-02 22:16:15 +0700

    first commit

---
+++ src/Tugas1Perulangan1to100.java
@@ -0,0 +1,11 @@
+public class Tugas1Perulangan1to100 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i <= 9) {
+                System.out.println(i);
+            } else {
+                System.out.println("Fidia Dewi  Wulandari Batu Bara");
+            }
+        }
+    }
+}
